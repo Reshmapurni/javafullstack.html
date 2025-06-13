9fb62030bbbd526daa2f66698c8b3e3243d03515
Reshmapurni <reshmapurniakula@gmail.com>
2025-06-13 09:21:25 +0530

    Add files via upload

---
+++ jslogin/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <legend>login form</legend>
+        <label for="">Email Id:<span>*</span></label>
+        <input type="email"name="" id="" placeholder="enter mail"></input>
+        <br></br>
+        <label>Password</label>
+        <input type="password"name=""id="" placeholder="password"></input>
+        <br></br>
+    </div>
+    </div>
+  );
+}
+
+export default App;
