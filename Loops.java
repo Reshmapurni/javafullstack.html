4e00cb9a2a3ec18cd55d94ae046bf95ad3f90582
Reshmapurni <reshmapurniakula@gmail.com>
2025-06-19 20:14:12 +0530

    Update Loops.java

--- Loops.java
+++ Loops.java
@@ -310,3 +310,37 @@ given number is not a prime number
 */
 
 -----------------------------------------------------------------
+    /*Loop 10
+    import java.util.Scanner;
+class Palindrome
+{
+public static void main(String[] args)
+{
+int r,sum=0;
+Scanner sc=new Scanner(System.in);
+System.out.println("enter a number");
+int n=sc.nextInt();
+int real=n;
+while(n>0)
+{
+r=(n%10);
+sum=sum*10+r;
+n=n/10;
+}
+if(real==sum)
+{
+System.out.println("given number is palindrome");
+}
+else
+{
+System.out.println("given number is not a palindrome");
+
+}
+}
+}
+Output:
+enter a number
+111
+given number is palindrome
+*/
+---------------------------------------------------------------------
