8ea4fdf8c3c59debac54be22fb34fc51728caa9d
Reshmapurni <reshmapurniakula@gmail.com>
2025-06-21 09:51:38 +0530

    Update Magical Container.java

--- Magical Container.java
+++ Magical Container.java
@@ -6,9 +6,10 @@ public static void main(String[] args)
 int N=5,A[]={1,2,3,4,5},k=0,B[]={0,0,0,0,0};
 for(int i=0;i<N;i++)
 {
+int r=A[i]-B[i];
 for(int j=i;j<N;j++)
 {
-B[j]=B[j]+1;
+B[j]=B[j]+r;
 }
 k++;
 }
