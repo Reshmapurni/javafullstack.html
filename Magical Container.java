f931d4bc6455f8a8346876ab0375f03db8766c2e
Reshmapurni <reshmapurniakula@gmail.com>
2025-06-21 06:27:10 +0530

    Create Magical Container.java

---
+++ Magical Container.java
@@ -0,0 +1,32 @@
+/*
+class QualityFactor
+{
+public static void main(String[] args)
+{
+int N=5,A[]={1,2,3,4,5},k=0,B[]={0,0,0,0,0};
+for(int i=0;i<N;i++)
+{
+for(int j=i;j<N;j++)
+{
+B[j]=B[j]+1;
+}
+k++;
+}
+System.out.println("magical array is:");
+for(int i=0;i<N;i++)
+{
+System.out.println(B[i]);
+}
+System.out.println(k);
+
+}
+}
+Output:
+magical array is:
+1
+2
+3
+4
+5
+5
+*/
