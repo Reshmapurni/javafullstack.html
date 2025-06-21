02ff7a76a5f90c8922229f44b69b7c4de3e00210
Reshmapurni <reshmapurniakula@gmail.com>
2025-06-21 12:11:34 +0530

    Update Magical Container.java

--- Magical Container.java
+++ Magical Container.java
@@ -3,7 +3,7 @@ class QualityFactor
 {
 public static void main(String[] args)
 {
-int N=5,A[]={1,2,3,4,5},k=0,B[]={0,0,0,0,0};
+int N=5,A[]={1,2,3,4,5},k=0,B[]={5,4,6,3,1};
 for(int i=0;i<N;i++)
 {
 int r=A[i]-B[i];
@@ -11,6 +11,11 @@ for(int j=i;j<N;j++)
 {
 B[j]=B[j]+r;
 }
+for(int l=0;l<N;l++)
+{
+System.out.print(B[l]+" ");
+}
+System.out.println();
 k++;
 }
 System.out.println("magical array is:");
@@ -23,6 +28,11 @@ System.out.println(k);
 }
 }
 Output:
+1 0 2 -1 -3
+1 2 4 1 -1
+1 2 3 0 -2
+1 2 3 4 2
+1 2 3 4 5
 magical array is:
 1
 2
