7fb99c3fe5a7359d2b7f6d235f624a28dc27d239
Reshmapurni <reshmapurniakula@gmail.com>
2025-06-22 09:11:07 +0530

    Update Static.java

--- Static.java
+++ Static.java
@@ -1,54 +1,79 @@
+/*we can access instance variables and instance methods when we create object locally or passing object as parameter to the static method */
+
+
+
 class Class3
 {
 int p=10;
 int q=20;
 static int x=100;
 static int y=200;
-{
-System.out.println(p+q);
-System.out.println("instance block1 of class2");
-}
-{
-System.out.println(q-p);
-System.out.println("instance block2 of class2");
-}
-static{
-System.out.println(x);
-System.out.println("static block1 of class2");
-}
-static{
-System.out.println(y);
-System.out.println("static block2 of class2");
-}
-void mul()
+void Print()
  {
-  System.out.println(p*q);
-  System.out.println("instance method1 of class2");
+  System.out.println("instance method1 of class3");
+  System.out.println(x);
+  System.out.println(y);
  }
-void div()
+void Display()
  {
-  System.out.println(p/q);
-  System.out.println("instance method2 of class2");
+  System.out.println("instance method2 of class3");
+  System.out.println(x);
+  System.out.println(y);
+  Display1();
  }
-static void mul1()
+static void Print1(Class3 obj)  //passing object as parameter
  {
-  System.out.println(x*y);
-  System.out.println("static method1 of class1");
+  System.out.println("static method1 of class3");
+  System.out.println(obj.p);
+  System.out.println(obj.q);
+  obj.Print();
+
  }
-static void div1()
+static void Display1()
  {
-  int a=40;
-  System.out.println(y/x);
-  System.out.println("static method2 of class1");
+  Class3 obj=new Class3();   //creating object locally
+  System.out.println("static method2 of class3");
+  System.out.println(obj.p);
+  System.out.println(obj.q);
  }
 public static void main(String[] args)
 {
-Class2 c3=new Class2();
-c3.mul();
-c3.div();
-c3.mul1();
-c3.div1();
-System.out.println(c2.p);
+Class3 c3=new Class3();
+Print1(c3);     //passing object as parameter
+Display1();
+c3.Display();
+c3.Print();
+System.out.println(c3.p);
+System.out.println(c3.q);
+System.out.println(c3.x);
+System.out.println(c3.y);
+
 }
 }
 
+
+/*
+Output:
+static method1 of class3
+10
+20
+instance method1 of class3
+100
+200
+static method2 of class3
+10
+20
+instance method2 of class3
+100
+200
+static method2 of class3
+10
+20
+instance method1 of class3
+100
+200
+10
+20
+100
+200
+ */
